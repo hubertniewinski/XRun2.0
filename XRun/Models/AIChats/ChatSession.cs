6044ccca65e03abb6b0d38ae36ab99e4788b2d6c
Hubert Niewiński <74657069+hubertniewinski@users.noreply.github.com>
2024-06-22 11:41:31 +0200

    update

--- XRun/Models/AIChats/ChatSession.cs
+++ XRun/Models/AIChats/ChatSession.cs
@@ -2,11 +2,11 @@ namespace XRun.Models.AIChats;
 
 public class ChatSession
 {
-    public required string Name { get; set; }
+    public string Name { get; set; }
     public List<SessionMessage> History { get; set; } = new List<SessionMessage>();
     public SessionStatus Status { get; set; } = SessionStatus.Active;
-    public required AIChat AIChat { get; set; }
-    public required Client Client { get; set; }
+    public AIChat AIChat { get; set; }
+    public Client Client { get; set; }
     
     public ChatSession(string name, AIChat aiChat, Client client)
     {
--- XRun/Models/AIChats/SessionMessage.cs
+++ XRun/Models/AIChats/SessionMessage.cs
@@ -2,9 +2,9 @@ namespace XRun.Models.AIChats;
 
 public class SessionMessage
 {
-    public required SessionMessageRole Role { get; set; }
-    public required IEnumerable<string> Parts { get; set; }
-    public required DateTime OccurredAt { get; set; } = DateTime.Now;
+    public SessionMessageRole Role { get; set; }
+    public IEnumerable<string> Parts { get; set; }
+    public DateTime OccurredAt { get; set; } = DateTime.Now;
     
     public SessionMessage(SessionMessageRole role, IEnumerable<string> parts)
     {
